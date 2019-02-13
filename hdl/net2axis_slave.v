b9dbc9b6468b7b54b6a5b5e85c6ed76d69572af8
Lucas Brasilino <lbrasili@indiana.edu>
2019-02-13 13:46:28 -0500

    proper flushing file

--- hdl/net2axis_slave.v
+++ hdl/net2axis_slave.v
@@ -28,33 +28,31 @@ module net2axis_slave
         );
 
     localparam                      IDLE = 0;
-    localparam                      WR_MD = 1;
-    localparam                      RD = 2;
-    localparam                      ADD_MD = 3;
-    localparam                      END = 4;
-    localparam                      POST_WR = 5;
-    localparam                      LAST = 6;
+    localparam                      RD = 1;
+    localparam                      DONE_TEST = 2;
+    localparam                      END = 3;
 
     localparam                      COUNTER_WIDTH = 16;
-    localparam                      STATE_WIDTH = $clog2(LAST);
+    localparam                      STATE_WIDTH = $clog2(END);
 
     integer                         fd, ld, errno;
 
     reg [STATE_WIDTH-1:0]             state, state_next;
-    wire                              done;
+    wire                              done,done_flag;
     wire [C_TDATA_WIDTH-1 : 0]        tdata;
     wire [(C_TDATA_WIDTH/8)-1 : 0]    tkeep;
     wire                              tvalid;
     wire                              tlast;
     reg                               tready;
     reg  [15:0]                       pkt_id;
+    reg                               done_r;
 
     assign tdata              = S_AXIS_TDATA;
     assign tkeep              = S_AXIS_TKEEP;
     assign tlast              = S_AXIS_TLAST;
     assign tvalid             = S_AXIS_TVALID;
     assign S_AXIS_TREADY      = tready;
-    assign DONE               =     done;
+    assign done               = DONE;
 
     initial begin
         $timeformat(-9, 2, " ns", 20);
@@ -81,15 +79,15 @@ module net2axis_slave
         tready = 1'b0;
         case (state)
             IDLE: begin
-                state_next = (S_AXIS_TVALID) ? WR_MD : IDLE;
+                tready = 1'b1;
+                state_next = (S_AXIS_TVALID) ? RD : IDLE;
             end
-            WR_MD: state_next =(S_AXIS_TVALID) ? RD : WR_MD;
             RD: begin
                 tready = 1'b1;
-                state_next = (S_AXIS_TVALID && S_AXIS_TLAST) ? ADD_MD : RD;
+                state_next = (S_AXIS_TVALID && S_AXIS_TLAST) ? DONE_TEST : RD;
             end
-            ADD_MD: state_next = END;
-            END: state_next = IDLE; /*will never go*/
+            DONE_TEST: state_next = (done_flag) ? END : IDLE;
+            END: state_next = END;
         endcase
     end
 
@@ -101,24 +99,20 @@ module net2axis_slave
     end
 
     always @(posedge ACLK) begin : WRITE_FILE
-        if (fd && (state==WR_MD)) begin
-            if (S_AXIS_TVALID) $fwrite("M: packet=%d delay=10",pkt_id);
-        end
-        if (fd && S_AXIS_TVALID  && (state==IDLE)) begin
-            $fwrite(fd, "%x,%x,%x\n",tdata,tkeep,tlast);
-            #1 $display("[%0t] %x | %x | %x | %x",$time,tvalid, tdata,tkeep,tlast);
+        if (fd && (state==IDLE)) begin
+            if (S_AXIS_TVALID) $fwrite(fd,"M: packet=%0d, delay=10\n",pkt_id);
         end
         if (fd && (state==RD)) begin
-            $fwrite(fd, "%x,%x,%x\n",tdata,tkeep,tlast);
+            if (S_AXIS_TVALID) begin
+                $fwrite(fd, "%x,%x,%x\n",tdata,tkeep,tlast);
             #1 $display("[%0t] %x | %x | %x | %x",$time,tvalid, tdata,tkeep,tlast);
-        end
-        if (fd && (state==ADD_MD)) begin
-            $fwrite(fd, "M: pkt=1, delay=10");
+            end
         end
         if (fd && (state==END)) begin
+            $display("[%0t] Flushing output file: %s",$time,C_OUTPUTFILE);
             $fflush(fd);
             $fclose(fd);
-            #1 $display("[%0t] simulation end",$time);
+            #1 $display("[%0t] netaxis2_slave: Simulation end",$time);
             $finish;
         end
     end//always
@@ -131,4 +125,15 @@ module net2axis_slave
             pkt_id <= pkt_id + 1'b1;
     end
 
+    assign done_flag = (done || done_r);
+    always @(posedge ACLK) begin : DONE_REG
+        if (~ARESETN)
+            done_r <= 1'b0;
+        else begin
+            if (state==DONE_TEST)
+                done_r <= 1'b0;
+            else if (done)
+                done_r <= 1'b1;
+        end
+    end
 endmodule
