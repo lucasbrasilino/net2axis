7ac3bd29fb53afcdc202388f80010f31a1b396ee
Lucas Brasilino <lbrasili@indiana.edu>
2019-06-16 11:29:37 -0400

    clog2 back

--- hdl/net2axis_master.v
+++ hdl/net2axis_master.v
@@ -29,7 +29,14 @@ module net2axis_master #(
         input wire                             M_AXIS_TREADY
         );
 
-    `include "funclib.vh"
+    function integer clog2;
+        input integer value;
+        begin
+            value = value-1;
+            for (clog2=0; value>0; clog2=clog2+1)
+                value = value>>1;
+        end
+    endfunction
 
     localparam                      IDLE = 0;
     localparam                      WAIT_START = 1;
