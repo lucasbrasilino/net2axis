1ddcb2718bbc89d926be69aae3db4a75a31ea8a8
Lucas Brasilino <lbrasili@indiana.edu>
2019-06-16 08:59:14 -0400

    updated BD example

--- tcl/net2axis_bd.tcl
+++ tcl/net2axis_bd.tcl
@@ -2,6 +2,9 @@ set design_name net2axis_bd
 set cur_design [current_bd_design -quiet]
 set list_cells [get_bd_cells -quiet]
 
+set_property  ip_repo_paths  [pwd] [current_project]
+update_ip_catalog -rebuild
+
 if { ${cur_design} eq "" } {
     create_bd_design $design_name
 }
@@ -14,9 +17,9 @@ set_property -dict [ list \
 ] $clk
 set sync_rst [ create_bd_port -dir O -type rst sync_rst ]
 
-set net2axis_0 [ create_bd_cell -type ip -vlnv lucasbrasilino.com:LB:net2axis_master:1.1 net2axis_0 ]
+set net2axis_0 [ create_bd_cell -type ip -vlnv lucasbrasilino.com:LB:net2axis_master:1.1 net2axis_master_0 ]
 set_property -dict [ list \
-    CONFIG.C_INPUTFILE {} \
+    CONFIG.INPUTFILE {} \
 ] $net2axis_0
 
 set sim_clk_gen_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:sim_clk_gen:1.0 sim_clk_gen_0 ]
@@ -24,8 +27,8 @@ set_property -dict [ list \
     CONFIG.INITIAL_RESET_CLOCK_CYCLES {10} \
 ] $sim_clk_gen_0
 
-connect_bd_intf_net -intf_net net2axis_0_M_AXIS [get_bd_intf_ports M_AXIS] [get_bd_intf_pins net2axis_0/M_AXIS]
+connect_bd_intf_net -intf_net net2axis_0_M_AXIS [get_bd_intf_ports M_AXIS] [get_bd_intf_pins net2axis_master_0/M_AXIS]
 
-connect_bd_net -net net2axis_0_DONE [get_bd_ports DONE] [get_bd_pins net2axis_0/DONE]
-connect_bd_net -net sim_clk_gen_0_clk [get_bd_ports clk] [get_bd_pins net2axis_0/ACLK] [get_bd_pins sim_clk_gen_0/clk]
-connect_bd_net -net sim_clk_gen_0_sync_rst [get_bd_ports sync_rst] [get_bd_pins net2axis_0/ARESETN] [get_bd_pins sim_clk_gen_0/sync_rst]
+connect_bd_net -net net2axis_0_DONE [get_bd_ports DONE] [get_bd_pins net2axis_master_0/DONE]
+connect_bd_net -net sim_clk_gen_0_clk [get_bd_ports clk] [get_bd_pins net2axis_master_0/ACLK] [get_bd_pins sim_clk_gen_0/clk]
+connect_bd_net -net sim_clk_gen_0_sync_rst [get_bd_ports sync_rst] [get_bd_pins net2axis_master_0/ARESETN] [get_bd_pins sim_clk_gen_0/sync_rst]
